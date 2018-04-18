2dadde1f33dc6b5b5b3597b29c382b0ae1a2c10e
Keroles Hakem <keroleshakem@gmail.com>
2018-04-17 22:50:40 -0400

    add hasuniqueChars problem

---
+++ hasUniqueChars.js
@@ -0,0 +1,16 @@
+function hasUniqueChar(str){
+let checker = 0;
+let a = 'a'.charCodeAt(0);
+for(let char of str){
+    let n = char.charCodeAt(0) - a;
+    if((checker & (1<<n))>0){
+        return false;
+    }
+    checker |= (1<<n);
+ }
+ return true;
+}
+
+
+console.log(hasUniqueChar("kerok"));
+console.log(hasUniqueChar("kero"));
